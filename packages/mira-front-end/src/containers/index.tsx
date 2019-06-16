7cb61c193d463f1acafb762acda05b499bb638a8
Dat Do <dat.b.do@gmail.com>
2019-06-16 16:08:24 -0400

    feat: initialize mira front end

---
+++ packages/mira-front-end/src/containers/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Project Mira</h1>
+      <h3>Smart Mirror on Budget</h3>
+    </div>
+  );
+};
+
+export default App;
